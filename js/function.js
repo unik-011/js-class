b384646d1fd402e7dfff789d7ea79d977b5087fa
unik-011 <bhandariunique6@gmail.com>
2025-02-15 16:32:10 +0545

    class

--- js/function.js
+++ js/function.js
@@ -1,11 +1,44 @@
-const pi = 3.14;
+// ES5
 
-function area(radius) {
-  const answer = pi * radius * radius;
-  return answer;
-}
+// const pi = 3.14;
 
-const rad = +prompt("enter the radius of circle");
+// function area(radius) {
+//   const answer = pi * radius * radius;
+//   return answer;
+// }
 
-const result = area(rad);
-alert(result);
+// const rad = +prompt("enter the radius of circle");
+
+// const result = area(rad);
+// alert(result);
+
+// ES6
+// Arrow function
+
+// const sum = (num1, num2) => {
+//   const result = num1 + num2;
+//   return result;
+// };
+
+// function call
+// console.log(sum(1, 2));
+
+// default function
+
+// const greet = (name = "user") => {
+//   return `welcome ${name}`;
+// };
+
+// console.log(greet("unique"));
+// console.log(greet());
+
+// parametrized function
+
+// const volume = ({ length, breadth, height }) => {
+//   return `The volume = ${+length * +breadth * +height}`;
+// };
+
+// console.log(volume({ length: "12", breadth: "10", height: "11" }));
+
+//IIFES FUNCTION (Immediately invoked functional expresion
+=
